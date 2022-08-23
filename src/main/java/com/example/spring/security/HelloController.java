3d2ebd01c143d13186e40d378e6473dbe2c0827a
Lucas Fernandez <lucasfernandezepe@gmail.com>
2022-08-22 22:01:26 -0300

    Initial commit

---
+++ src/main/java/com/example/spring/security/HelloController.java
@@ -0,0 +1,39 @@
+package com.example.spring.security;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+    @GetMapping("/hola")
+    public String holaMundo(){
+        System.out.println(message);
+        return "Hola mundo!! que tal!!";
+    }
+
+
+    @GetMapping("/bootstrap")
+    public String bootstrap(){
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hello, world!</h1>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                """;
+    }
+
+}
